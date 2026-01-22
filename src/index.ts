88c1eed01bdb7b9842bfa173232a2f81671410d8
Imduff <erickbrad10@gmail.com>
2026-01-22 19:15:19 -0300

    router finished

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,9 @@
 import express from "express"
-import { prisma } from "./prisma/prisma.js"
-import { get } from "node:http"
-import repository from "./repository/solicitante.repository.js"
-import controller from "./controller/solicitante.controller.js"
+import {router} from "./router.js"
 const app = express()
 
 app.use(express.json())
-
-app.post("/addorder", controller.addOrder)
-app.get("/", controller.getOrder)
-app.put("/update/:id", controller.updateOrder)
-app.delete("/delete/:id", controller.deleteOrder)
-
+app.use(router)
 
 export {app}
 
